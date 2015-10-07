f2855b783d8cc21e4dffe7420821392d495d76bc
Henrik Blidh <henrik.blidh@swedwise.com>
2015-10-07 10:09:16 +0200

    Version 0.2.0 Refactored to eliminate numpy dependency.

---
+++ hbldhdoku/utils.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+:mod:`utils`
+==================
+
+.. module:: utils
+    :platform: Unix, Windows
+    :synopsis:
+
+.. moduleauthor:: hbldh <henrik.blidh@nedomkull.com>
+
+Created on 2015-10-07, 09:22
+
+"""
+
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+from __future__ import absolute_import
+
+import six
+
+
+def get_list(n_rows, fill_with=0.0):
+    return [fill_with, ] * n_rows
+
+
+def get_list_of_lists(n_rows, n_cols, fill_with=0):
+    return [get_list(n_rows, fill_with) for k in six.moves.range(n_cols)]
